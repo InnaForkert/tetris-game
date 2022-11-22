148cde51d180a3edb1ae279a6574fcda430be9b3
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-11-22 18:52:28 +0200

    rocket

--- src/scripts/playground.js
+++ src/scripts/playground.js
@@ -1,23 +1,11 @@
-export const exportFunction = () => {};
-
-const playground = document.getElementById('playground');
-
-playground.addEventListener('mouseover', selectChosen);
-playground.addEventListener('mouseout', unselectChosen);
-
-function selectChosen(e) {
-  e.target.classList.add('chosen-div');
-}
-
-function unselectChosen(e) {
-  e.target.classList.remove('chosen-div');
-}
+const playground = document.querySelector('#playground');
 
 (function createField() {
   const divs = [];
   for (let i = 0; i < 3250; i += 1) {
     const smallDiv = document.createElement('div');
     smallDiv.classList.add('small-div');
+    smallDiv.dataset.id = i;
     divs.push(smallDiv);
   }
   playground?.append(...divs);
