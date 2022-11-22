5bf26e5387e18e6511b2f1668cfd299435918861
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-11-22 20:21:38 +0200

    undefined div console fix

--- src/scripts/playground.js
+++ src/scripts/playground.js
@@ -1,5 +1,5 @@
 const playground = document.querySelector('#playground');
-
+//створюємо поле, щоб монжа було окремо звертатись до кожного квадратика
 (function createField() {
   const divs = [];
   for (let i = 0; i < 3250; i += 1) {
@@ -8,5 +8,5 @@ const playground = document.querySelector('#playground');
     smallDiv.dataset.id = i;
     divs.push(smallDiv);
   }
-  playground?.append(...divs);
+  playground.append(...divs);
 })();
