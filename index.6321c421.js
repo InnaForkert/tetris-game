a817887987d148a0cac2447f1560926e58cf28f9
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-12-14 16:29:30 +0000

    Deploying to gh-pages from @ InnaForkert/tetris-game@1369d7a8dadd275879260a11d97205df57d1c325 🚀

---
+++ index.6321c421.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("#playground");!function(){const e=[];for(let t=0;t<3250;t+=1){const d=document.createElement("div");d.classList.add("small-div"),d.dataset.id=t,e.push(d)}null==t||t.append(...e);const d=document.querySelectorAll(".small-div");d.forEach(((t,e)=>{Math.random(),e<1700&&e>300&&t.classList.add("chosen-div")}))}();
+//# sourceMappingURL=index.6321c421.js.map
--- index.92f21643.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("#playground");!function(){const e=[];for(let t=0;t<3250;t+=1){const d=document.createElement("div");d.classList.add("small-div"),d.dataset.id=t,e.push(d)}t.append(...e)}();
-//# sourceMappingURL=index.92f21643.js.map
