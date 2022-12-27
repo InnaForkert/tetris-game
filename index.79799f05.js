3ab09382efa77c34e6fe1486e511603b71ceed4b
YuliiaDikun <107618913+YuliiaDikun@users.noreply.github.com>
2022-12-27 07:48:09 +0000

    Deploying to gh-pages from @ InnaForkert/tetris-game@9bc38020bbb43092d9c5baa8fa895c7094f4835c 🚀

---
+++ index.79799f05.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#playground");!function(){const t=[];for(let e=0;e<3250;e+=1){const d=document.createElement("div");d.classList.add("small-div"),d.dataset.id=e,t.push(d)}null==e||e.append(...t);document.querySelectorAll(".small-div").forEach(((e,t)=>{Math.random()>.6&&t<2500&&t>1500&&e.classList.add("chosen-div")}))}();
+//# sourceMappingURL=index.79799f05.js.map
