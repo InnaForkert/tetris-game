b696bc90e54a2e3bb7d27e62f08316df9667c5dd
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2023-01-09 03:44:36 +0200

    add sounds

---
+++ src/scripts/sounds.js
@@ -0,0 +1,7 @@
+export const sounds = {
+  hitBlock: new Audio(require('url:../sounds/hit_block.wav')),
+  hitRocket: new Audio(require('url:../sounds/hit_rocket.wav')),
+  hitWall: new Audio(require('url:../sounds/hit_wall.wav')),
+  start: new Audio(require('url:../sounds/start.wav')),
+  lost: new Audio(require('url:../sounds/lost.wav')),
+};
